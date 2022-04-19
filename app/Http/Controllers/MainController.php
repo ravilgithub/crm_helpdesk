2613dd6e3cce5b3c486020c38023de80881ecd6b
Ravil <foragetonly800@mail.ru>
2022-04-20 01:30:34 +0300

    Laravel&Vue CRM.2: Postman, Router, Controller. https://youtu.be/n3TsfHt6JQk

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MainController extends Controller
+{
+    public function index() {
+        return view( 'welcome' );
+    }
+
+    public function form_post() {
+        return response()->json( [ 'name' => 'Ravil', 'age' => 41 ], 201 );
+    }
+
+    public function form_put() {
+        return response()->json( [ 'name' => 'Ravil', 'age' => 42 ] );
+    }
+
+    public function form_any() {
+        return response()->json( [ 'always' => 'response' ] );
+    }
+
+    // Возвращает строку и статус 201
+    public function html() {
+        return response( 'response html', 201 );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ MainController::class, 'index' ] );
+
+Route::post( 'form', [ MainController::class, 'form_post' ] );
+Route::put( 'form', [ MainController::class, 'form_put' ] );
+// Route::any( 'form', [ MainController::class, 'form_any' ] );
+
+Route::get( 'html', [ MainController::class, 'html' ] );
