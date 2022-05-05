e49ac05e3b09248bcf4471e306c25107516cde4c
Ravil <foragetonly800@mail.ru>
2022-05-05 07:15:27 +0300

    Laravel&Vue CRM.7: Начальные данные приложения (Seeds). https://youtu.be/w_3jPeFsVNQ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,17 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
+     * https://laravel.su/docs/8.x/seeding
+     * В консоль: php artisan migrate:fresh --seed
+     *
      * @return void
      */
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call( [
+            RolesSeeder::class
+        ] );
     }
 }
