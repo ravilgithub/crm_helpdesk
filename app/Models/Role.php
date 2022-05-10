fd5cf2676fc6211716e85ab5d19a934917e81551
Ravil <foragetonly800@mail.ru>
2022-05-10 18:14:39 +0300

    Laravel&Vue CRM.8: Связи между моделями. Вторая часть. https://youtu.be/NP0-SdzVFSU

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,19 +9,28 @@ class Role extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-    ];
 
     /**
      * Атрибуты, для которых НЕ разрешено массовое присвоение значений.
      *
      * @var array
      */
-    // protected $guarded = [];
+    protected $guarded = [];
+
+
+    /**
+     * Get users.
+     *
+     * https://laravel.su/docs/8.x/eloquent-relationships#one-to-many
+     *
+     * @see App\Http\Controllers\RolesController::users
+     *
+     * SQL:
+     * SELECT u.* FROM users AS u JOIN roles AS r ON u.role_id = r.id WHERE r.id = 2;
+     */
+    public function users() {
+        // Class, foreignKey, localKey
+        // return $this->hasMany( User::class, 'role_id', 'id' );
+        return $this->hasMany( User::class );
+    }
 }
