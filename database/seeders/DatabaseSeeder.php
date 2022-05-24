63de57092e9d13ec6bcdb7da74b5e067ab1fdd6c
Ravil <foragetonly800@mail.ru>
2022-05-24 18:19:45 +0300

    Laravel&Vue CRM.9: Фабрики, наполнение контентом. https://youtu.be/X_jCt7-_ibI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Ticket;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +18,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Создаём пользователей, но не вносим записи в таблицу.
+        // Можем посмотреть структуру модели перед вставкой в таблицу, а в частности атрибуты: #attributes, #fillable, #hidden, #casts.
+        /*$user = User::factory()->make();
+        dd( $user );*/
 
-        $this->call( [
+        // Создаём пользователей в таблице "User".
+        // User::factory( 2 )->create();
+
+
+
+        /**
+         * Создаём одного пользователя с ролью "admin".
+         */
+        // Вариант 1
+        /*User::factory( 1 )->create( [
+            'role_id' => 1
+        ] );*/
+        // Вариант 2
+        // @see Database\Factories\UserFactory
+        User::factory( 1 )->admin()->create();
+
+
+
+        /*$tickets = Ticket::factory( 3 )->make();
+        dd( $tickets );*/
+
+        // Ticket::factory( 3 )->create();
+
+
+        // https://laravel.su/docs/8.x/database-testing#has-many-relationships
+        User::factory( 3 )
+            ->client()
+            // ->has( Ticket::factory()->count( 3 ) )
+            ->hasTickets( 3 ) // короткая запись
+            ->create();
+
+
+        /*$this->call( [
             RolesSeeder::class
-        ] );
+        ] );*/
     }
 }
