4450a9d40856e186d6d3802451b294b1f7fba891
Ravil <foragetonly800@mail.ru>
2022-11-30 20:24:44 +0300

    Laravel&Vue CRM.16: Двухстороннее связывание, Списки, События. https://youtu.be/pidMo7BWv_Y

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,22 +1,61 @@
 <template lang="pug">
-h1 Crm Helpdesk!
-.box
-    p.active {{ greating }}
+
+h1= title
+
+input(
+    type='text',
+    v-model='title'
+)
+
+ul
+    li(
+        v-for='user in users',
+        :key='getKey()'
+    )= user
+
+input(
+    type='text'
+    v-model='name'
+)
+
+button.btn(
+    @click='addUser'
+) Add user
+
 </template>
 
 <script>
-export default {
-    name: "AppHome",
-    data() {
-        return {
-            greating: 'Hello, World!'
-        }
+    export default {
+        name: "AppHome",
+        data() {
+            return {
+                title: 'Crm Helpdesk!',
+                users: [
+                    'John Smith',
+                    'Mike Dow',
+                    'Jane Dow',
+                ],
+                name: null,
+            }
+        },
+
+        methods: {
+            getKey() {
+                return Math.random().toString( 36 ).slice( -10 )
+            },
+
+            addUser() {
+                if ( this.name ) {
+                    this.users.push( this.name )
+                    this.name = null
+                }
+            },
+        },
     }
-}
 </script>
 
 <style lang="sass" scoped>
-    $baseColor: blue
+    $baseColor: #5830ea
     $baseBgColor: #d7d7d7
 
     *
